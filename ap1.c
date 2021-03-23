7c36020af30b32dfa1a56c9ee9eea19a1e8ab963
Intae0099 <81225723+Intae0099@users.noreply.github.com>
2021-03-23 21:01:20 +0900

    Update ap1.c

--- ap1.c
+++ ap1.c
@@ -3,38 +3,41 @@
 
 void main()
 {
-    int list[5];
-    int *plist[5] = {NULL,};
+    printf("[----- [김태인] [2018038033] -----]\n");
 
-    plist[0] = (int *)malloc(sizeof(int));
+    int list[5]; //5칸 짜리 배열 list[5] 선언
+    int *plist[5] = {NULL, }; //5칸 짜리 포인터형 배열 *plist[5] 선언
 
-    list[0] = 1;
-    list[1] = 100;
+    plist[0] = (int *)malloc(sizeof(int)); //plist[0]을 int 사이즈만큼 동적할당
 
-    *plist[0] = 200;
+    list[0] = 1; //list[0]에 1 설정
+    list[1] = 100; //list[1]에 100 설정
 
-    printf("value of list[0] = %d\n", list[0]);
-    printf("address of list[0]        = %p\n", &list[0]);
-    printf("value of list             = %p\n", list);
-    printf("address of list (&list)   = %p\n", &list);
+    *plist[0] = 200; //*plist[0]에 200 설정
 
-    printf("---------------------------------------------\n\n");
-    printf("value of list[1]          = %d\n", list[1]);
-    printf("address of list[1]        = %p\n", &list[1]);
-    printf("value of *(list+1)        = %d\n", *(list + 1));
-    printf("address of list+1         = %p\n", list +1);
+    printf("value of list[0] = %d\n", list[0]); //list[0] 값 출력
+    printf("address of list[0]        = %p\n", &list[0]); //list[0]의 주소 출력
+    printf("value of list             = %p\n", list); //list값 출력
+    printf("address of list (&list)   = %p\n", &list); //list의 주소값 출력
 
-    printf("---------------------------------------------\n\n");
+    printf("---------------------------------------------\n\n"); //나눔선
+    printf("value of list[1]          = %d\n", list[1]); //list[1]의 값 출력
+    printf("address of list[1]        = %p\n", &list[1]); //list[1]의 주소값 출력
+    printf("value of *(list+1)        = %d\n", *(list + 1)); //*(list + 1)의 값 출력
+    printf("address of list+1         = %p\n", list +1); //list + 1 의 주소값 출력
 
-    printf("value of *plist[0] = %d\n", *plist[0]);
-    printf("&plist[0]          = %p\n", &plist[0]);
-    printf("&plist             = %p\n", &plist);
-    printf("plist              = %p\n", plist);
-    printf("plist[0]           = %p\n", plist[0]);
-    printf("plist[1]           = %p\n", plist[1]);
-    printf("plist[2]           = %p\n", plist[2]);
-    printf("plist[3]           = %p\n", plist[3]);
-    printf("plist[4]           = %p\n", plist[4]);
+    printf("---------------------------------------------\n\n"); //나눔선
 
-    free(plist[0]);
+    printf("value of *plist[0] = %d\n", *plist[0]); //*plist[0]값 출력
+    printf("&plist[0]          = %p\n", &plist[0]); //*plist[0]의 주소값 출력
+    printf("&plist             = %p\n", &plist); //plist의 주소값 출력
+    printf("plist              = %p\n", plist); //plist 값 출력
+    printf("plist[0]           = %p\n", plist[0]); //plist[0]의 값 출력
+    printf("plist[1]           = %p\n", plist[1]); //plist[1]의 값 출력
+    printf("plist[2]           = %p\n", plist[2]); //plist[2]의 값 출력
+    printf("plist[3]           = %p\n", plist[3]); //plist[3]의 값 출력
+    printf("plist[4]           = %p\n", plist[4]); //plist[4]의 값 출력
+
+    free(plist[0]); //plist[0]의 동적할당 해제
+    
 }
